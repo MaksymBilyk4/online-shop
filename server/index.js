3b158fa72defc13827076a695ceef30c60bee913
Max Bilyk <datan0612@gmail.com>
2022-07-16 02:20:36 +0300

    routing, controllers, error handling

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+const router = require('./routes/index');
+const errorHandler = require("./middlware/ErrorHandlingMiddleware");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
+
+app.use(errorHandler)
 
 const start = async () => {
     try{
